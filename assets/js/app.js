7830a86e76f5d548535837111208160420204010
scha-chan <schasta@gmail.com>
2017-06-04 19:18:13 -0300

    Front-end bootstrap

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+    $('.carousel').carousel({
+	  interval: 2000
+	});
+	// $( ".nav-pills li" ).on('click',function() {
+	//   $('.tabs .tab-content .tab-pane.active').hide();
+	//   $('.tabs .tab-content .tab-pane').removeClass('active');
+	//   var id = $(this).find('a').first().attr('href');	  
+	//   $(id).show('slide', {direction: 'left'}, 1500);
+	//   $(id).addClass('active');
+	// });
+});
